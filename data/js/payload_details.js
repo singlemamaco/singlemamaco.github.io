20f828f2376cb4958bced9d08a8dbf28b21ec466
Amy Madeline  |  Single Mama Design Co <singlemamaco@users.noreply.github.com>
2019-03-08 19:10:45 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23245,
-	"created_at": "Fri Mar 08 00:10:45 +0000 2019",
+	"tweets": 23246,
+	"created_at": "Sat Mar 09 00:10:44 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Fri Mar 08 17:07:02 +0000 2019",
+		"id": 1104066004290658300,
+		"id_str": "1104066004290658304",
+		"full_text": "Creative Bloq's Job of the Week https://t.co/I6urWjiJAT https://t.co/nic6mhlVOc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			55
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/I6urWjiJAT",
+					"expanded_url": "http://dlvr.it/R0S0rT",
+					"display_url": "dlvr.it/R0S0rT",
+					"indices": [
+						32,
+						55
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1104066002336116700,
+					"id_str": "1104066002336116736",
+					"indices": [
+						56,
+						79
+					],
+					"media_url": "http://pbs.twimg.com/media/D1JuLfXUcAAahul.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1JuLfXUcAAahul.jpg",
+					"url": "https://t.co/nic6mhlVOc",
+					"display_url": "pic.twitter.com/nic6mhlVOc",
+					"expanded_url": "https://twitter.com/singlemamaco/status/1104066004290658304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 600,
+							"h": 192,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 192,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 600,
+							"h": 192,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1104066002336116700,
+					"id_str": "1104066002336116736",
+					"indices": [
+						56,
+						79
+					],
+					"media_url": "http://pbs.twimg.com/media/D1JuLfXUcAAahul.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1JuLfXUcAAahul.jpg",
+					"url": "https://t.co/nic6mhlVOc",
+					"display_url": "pic.twitter.com/nic6mhlVOc",
+					"expanded_url": "https://twitter.com/singlemamaco/status/1104066004290658304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 600,
+							"h": 192,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 192,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 600,
+							"h": 192,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 703680978879979500,
+			"id_str": "703680978879979521",
+			"name": "Single Mama Design Co",
+			"screen_name": "singlemamaco",
+			"location": "Easthampton, MA",
+			"description": "celebrating all things #design & #DIY #graphicdesign #branding #solopreneur #webdesign #architecture #typography #interiordesign #socialmedia",
+			"url": "https://t.co/87Uuz3LtL6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/87Uuz3LtL6",
+							"expanded_url": "http://singlemamadesignco.com",
+							"display_url": "singlemamadesignco.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1104,
+			"friends_count": 1129,
+			"listed_count": 406,
+			"created_at": "Sat Feb 27 20:39:53 +0000 2016",
+			"favourites_count": 142,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23217,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/877764065204068352/7UuHX6EU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/877764065204068352/7UuHX6EU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/703680978879979521/1486017084",
+			"profile_link_color": "E78888",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Creative Bloq's Job of the Week https://t.co/I6urWjiJAT https://t.co/nic6mhlVOc"
+	},
+	{
 		"created_at": "Fri Mar 08 00:06:02 +0000 2019",
 		"id": 1103809061290573800,
 		"id_str": "1103809061290573825",
