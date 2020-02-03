0d4ccb1165c81bb856ec83c239653ffa678ad3fa
Amy Madeline  |  Single Mama Design Co <singlemamaco@users.noreply.github.com>
2020-02-02 19:10:45 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23516,
-	"created_at": "Sun Feb 02 00:10:44 +0000 2020",
+	"tweets": 23517,
+	"created_at": "Mon Feb 03 00:10:44 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,263 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sun Feb 02 22:56:33 +0000 2020",
+		"id": 1224104344561184800,
+		"id_str": "1224104344561184769",
+		"full_text": "The Chap ? Digital Technology https://t.co/YboFJqwAes https://t.co/Hxp9WDEwxe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			53
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YboFJqwAes",
+					"expanded_url": "http://dlvr.it/RPFvch",
+					"display_url": "dlvr.it/RPFvch",
+					"indices": [
+						30,
+						53
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1224104341650342000,
+					"id_str": "1224104341650341888",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/EPzkacQVAAA-Qhb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPzkacQVAAA-Qhb.jpg",
+					"url": "https://t.co/Hxp9WDEwxe",
+					"display_url": "pic.twitter.com/Hxp9WDEwxe",
+					"expanded_url": "https://twitter.com/singlemamaco/status/1224104344561184769/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1400,
+							"h": 1395,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 678,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1196,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 51,
+									"y": 49,
+									"h": 801,
+									"w": 801
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 24,
+									"y": 23,
+									"h": 389,
+									"w": 389
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 51,
+									"y": 49,
+									"h": 801,
+									"w": 801
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 43,
+									"y": 42,
+									"h": 686,
+									"w": 686
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1224104341650342000,
+					"id_str": "1224104341650341888",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/EPzkacQVAAA-Qhb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPzkacQVAAA-Qhb.jpg",
+					"url": "https://t.co/Hxp9WDEwxe",
+					"display_url": "pic.twitter.com/Hxp9WDEwxe",
+					"expanded_url": "https://twitter.com/singlemamaco/status/1224104344561184769/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1400,
+							"h": 1395,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 678,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1196,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 51,
+									"y": 49,
+									"h": 801,
+									"w": 801
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 24,
+									"y": 23,
+									"h": 389,
+									"w": 389
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 51,
+									"y": 49,
+									"h": 801,
+									"w": 801
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 43,
+									"y": 42,
+									"h": 686,
+									"w": 686
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 703680978879979500,
+			"id_str": "703680978879979521",
+			"name": "Single Mama Design Co",
+			"screen_name": "singlemamaco",
+			"location": "Easthampton, MA",
+			"description": "celebrating all things #design & #DIY #graphicdesign #branding #solopreneur #architecture #typography #interiordesign #socialmedia #homestaging #realestate",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1050,
+			"friends_count": 1107,
+			"listed_count": 381,
+			"created_at": "Sat Feb 27 20:39:53 +0000 2016",
+			"favourites_count": 140,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23619,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/877764065204068352/7UuHX6EU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/877764065204068352/7UuHX6EU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/703680978879979521/1486017084",
+			"profile_link_color": "E78888",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The Chap ? Digital Technology https://t.co/YboFJqwAes https://t.co/Hxp9WDEwxe"
+	},
+	{
 		"created_at": "Sat Feb 01 20:01:02 +0000 2020",
 		"id": 1223697789902712800,
 		"id_str": "1223697789902712832",
