81a1a64db5c78d50809db8ef282e1c30f23aa7a4
Amy Madeline  |  Single Mama Design Co <singlemamaco@users.noreply.github.com>
2020-06-23 20:10:46 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 23756,
-	"created_at": "Tue Jun 23 00:10:45 +0000 2020",
+	"tweets": 23757,
+	"created_at": "Wed Jun 24 00:10:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,177 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Tue Jun 23 18:45:03 +0000 2020",
+		"id": 1275500130016178200,
+		"id_str": "1275500130016178179",
+		"full_text": "Clever Ep. 121: People-Centered Designer Marquise Stillwell https://t.co/FA5r499Drs https://t.co/0JfQroCFfk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			83
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/FA5r499Drs",
+					"expanded_url": "http://dlvr.it/RZD1G1",
+					"display_url": "dlvr.it/RZD1G1",
+					"indices": [
+						60,
+						83
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1275500128166539300,
+					"id_str": "1275500128166539264",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/EbN8m9JU8AA5-EC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EbN8m9JU8AA5-EC.jpg",
+					"url": "https://t.co/0JfQroCFfk",
+					"display_url": "pic.twitter.com/0JfQroCFfk",
+					"expanded_url": "https://twitter.com/singlemamaco/status/1275500130016178179/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1280,
+							"h": 1173,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 623,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1100,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1275500128166539300,
+					"id_str": "1275500128166539264",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/EbN8m9JU8AA5-EC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EbN8m9JU8AA5-EC.jpg",
+					"url": "https://t.co/0JfQroCFfk",
+					"display_url": "pic.twitter.com/0JfQroCFfk",
+					"expanded_url": "https://twitter.com/singlemamaco/status/1275500130016178179/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1280,
+							"h": 1173,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 623,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1100,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 703680978879979500,
+			"id_str": "703680978879979521",
+			"name": "Single Mama Design Co",
+			"screen_name": "singlemamaco",
+			"location": "Easthampton, MA",
+			"description": "celebrating all things #design & #DIY #graphicdesign #branding #solopreneur #architecture #typography #interiordesign #socialmedia #homestaging #realestate",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1022,
+			"friends_count": 1089,
+			"listed_count": 368,
+			"created_at": "Sat Feb 27 20:39:53 +0000 2016",
+			"favourites_count": 139,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 23975,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/877764065204068352/7UuHX6EU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/877764065204068352/7UuHX6EU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/703680978879979521/1486017084",
+			"profile_link_color": "E78888",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Clever Ep. 121: People-Centered Designer Marquise Stillwell https://t.co/FA5r499Drs https://t.co/0JfQroCFfk"
+	},
+	{
 		"created_at": "Mon Jun 22 18:47:33 +0000 2020",
 		"id": 1275138370864181200,
 		"id_str": "1275138370864181248",
