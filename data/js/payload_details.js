50a05762b081a0ec0b4a88e5ebe8e082c351fde4
Amy Madeline  |  Single Mama Design Co <singlemamaco@users.noreply.github.com>
2021-08-01 20:10:45 -0400

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 24409,
-	"created_at": "Sun Aug 01 00:10:45 +0000 2021",
+	"tweets": 24410,
+	"created_at": "Mon Aug 02 00:10:44 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_08.js
@@ -0,0 +1,175 @@
+Grailbird.data.tweets_2021_08 = 
+[
+	{
+		"created_at": "Sun Aug 01 19:25:34 +0000 2021",
+		"id": 1421915025552203800,
+		"id_str": "1421915025552203778",
+		"full_text": "A Furnished Artist’s Compound in the Woods of Vermont Lists for $1.2M https://t.co/FrJG6kd3yV https://t.co/aqc12MYl0P",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/FrJG6kd3yV",
+					"expanded_url": "http://dlvr.it/S4rRMN",
+					"display_url": "dlvr.it/S4rRMN",
+					"indices": [
+						70,
+						93
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1421915023081808000,
+					"id_str": "1421915023081807872",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/E7uoLguVIAAK8fn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7uoLguVIAAK8fn.jpg",
+					"url": "https://t.co/aqc12MYl0P",
+					"display_url": "pic.twitter.com/aqc12MYl0P",
+					"expanded_url": "https://twitter.com/singlemamaco/status/1421915025552203778/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 1067,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1421915023081808000,
+					"id_str": "1421915023081807872",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/E7uoLguVIAAK8fn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E7uoLguVIAAK8fn.jpg",
+					"url": "https://t.co/aqc12MYl0P",
+					"display_url": "pic.twitter.com/aqc12MYl0P",
+					"expanded_url": "https://twitter.com/singlemamaco/status/1421915025552203778/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 1067,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 703680978879979500,
+			"id_str": "703680978879979521",
+			"name": "Single Mama Design Co",
+			"screen_name": "singlemamaco",
+			"location": "Easthampton, MA",
+			"description": "celebrating all things #design & #DIY #graphicdesign #branding #solopreneur #architecture #typography #interiordesign #socialmedia #homestaging #realestate",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 988,
+			"friends_count": 1069,
+			"listed_count": 356,
+			"created_at": "Sat Feb 27 20:39:53 +0000 2016",
+			"favourites_count": 137,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 24948,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/877764065204068352/7UuHX6EU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/877764065204068352/7UuHX6EU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/703680978879979521/1486017084",
+			"profile_link_color": "E78888",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A Furnished Artist’s Compound in the Woods of Vermont Lists for $1.2M https://t.co/FrJG6kd3yV https://t.co/aqc12MYl0P"
+	}
+]
